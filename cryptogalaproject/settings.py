2b9a4ce1e920f455d84327409f01199189dcfc78
bruks <jenake8@gmail.com>
2022-03-21 19:39:20 +0100

    middleware

--- cryptogalaproject/settings.py
+++ cryptogalaproject/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.whiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
